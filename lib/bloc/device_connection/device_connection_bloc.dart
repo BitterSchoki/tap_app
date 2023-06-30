8aae31957d27bb21562e86b28d3a88fe476eb6fa
rpgraffi <rpgmelone@gmail.com>
2023-06-30 10:37:31 +0200

    Added tcp connection

--- lib/bloc/device_connection/device_connection_bloc.dart
+++ lib/bloc/device_connection/device_connection_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:tcp_socket_connection/tcp_socket_connection.dart';
 
 part 'device_connection_event.dart';
 part 'device_connection_state.dart';
@@ -13,17 +14,46 @@ class DeviceConnectionBloc
     });
   }
 
+  TcpSocketConnection? socketConnection;
+
+  void messageReceived() {
+    //TODO: message receiving
+  }
+
   Future<void> _deviceConnectionStarted(
       Emitter<DeviceConnectionState> emitter) async {
     emitter(DeviceConnectionInProgress());
 
+    //TODO: network discovery implement
+
+    const ip = "192.168.1.113";
+    const port = 43534;
+
+    socketConnection = TcpSocketConnection(ip, port);
+
+    socketConnection!.enableConsolePrint(true);
+
     try {
-      //TODO: Implement real connection
-      await Future<void>.delayed(const Duration(seconds: 2));
+      final canConnect = await socketConnection!.canConnect(5000, attempts: 3);
+
+      if (canConnect) {
+        await socketConnection!.connect(5000, messageReceived, attempts: 3);
+        socketConnection!.isConnected();
+      } else {
+        throw Error();
+      }
 
       emitter(DeviceConnectionSuccess());
     } catch (error) {
       emitter(DeviceConnectionFailure());
+    }
+  }
+
+  void sendMessage(String message) {
+    if (socketConnection!.isConnected()) {
+      socketConnection!.sendMessage(message);
+    } else {
+      //throw NoConnectionError();
     }
   }
 }
